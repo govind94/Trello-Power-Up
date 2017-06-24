de6aabe6ae313edd942f039167b02381a04d1fb4
Govindan Vedanarayanan <govind@Govind-Air.home>
2017-06-23 21:45:57 -0400

    final touches

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use(cors({ origin: 'https://trello.com' }));
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
