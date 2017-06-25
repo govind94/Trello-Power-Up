b2d75a0b2e6132bcae6fd38e8cae92422cb0e310
Govindan Vedanarayanan <govind@Govind-Air.home>
2017-06-24 23:32:22 -0400

    changes

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-var cors = require('cors');
 var express = require('express');
 var app = express();
+var cors = require('cors');
 
+// Set port based on environment variable
+// https://nodejs.org/api/process.html#process_process_env
+app.set('port', (process.env.PORT || 5000));
+
+// Allow Trello to make requests
+// https://github.com/expressjs/cors#simple-usage-enable-all-cors-requests
 app.use(cors({ origin: 'https://trello.com' }));
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+// Serve static files out of the `public` directory
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
